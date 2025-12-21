e82658ad01684e602d632b8e6abecce91a891d82
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-12-21 15:15:24 +0300

    import alias

--- packages/backend/src/modules/projects/infrastructure/persistence/repositories/test-repo.ts
+++ packages/backend/src/modules/projects/infrastructure/persistence/repositories/test-repo.ts
@@ -1,6 +1,6 @@
 import { eq, sql } from "drizzle-orm";
-import { DbClient } from "../../../../../app/db/connector";
-import { projects } from "../../../../../app/db/schema";
+import type { DbClient } from "@backend/app/db/connector";
+import { projects } from "@backend/app/db/schema";
 
 export class TestRepo {
   constructor(private readonly db: DbClient) {}
@@ -11,6 +11,7 @@ export class TestRepo {
       .from(projects)
       .where(eq(projects.id, id));
 
-    return result[0].count > 0;
+    const count = result[0]?.count;
+    return count !== undefined ? count > 0 : false;
   }
 }
