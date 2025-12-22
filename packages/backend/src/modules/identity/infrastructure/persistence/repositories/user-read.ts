8fb4912a70481c597f9c2f81f805b069c9bce752
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-12-22 03:20:18 +0300

    create identity repository

---
+++ packages/backend/src/modules/identity/infrastructure/persistence/repositories/user-read.ts
@@ -0,0 +1,68 @@
+import { eq } from 'drizzle-orm';
+import { Result } from '@backend/libs/result';
+import type { DbClient } from '@backend/app/db/connector';
+import { users } from '@backend/app/db/schema';
+import { UserSchema, type User } from '../../../domain/entities/user';
+import type { UserId } from '../../../domain/value-objects/user-id';
+import type { Email } from '../../../domain/value-objects/email';
+import type { UserReadRepository } from '../../../domain/repositories/user-read';
+import {
+	createInvalidObjectInDatabaseError,
+  createUnexpectedDatabaseError,
+  createUserNotFoundError,
+  type IdentityDomainError,
+	type InvalidObjectInDatabaseError,
+} from '../../../domain/errors';
+import { validate } from '@backend/libs/validation';
+
+export class DrizzleUserReadRepository implements UserReadRepository {
+  constructor(private readonly db: DbClient) {}
+
+  async findById(id: UserId): Promise<Result<User, IdentityDomainError>> {
+    try {
+      const userRecord = await this.db
+        .select()
+        .from(users)
+        .where(eq(users.id, id))
+        .limit(1);
+
+      if (userRecord.length === 0) {
+        return Result.err(createUserNotFoundError(id));
+      }
+
+       const validatedUser = validate<User, InvalidObjectInDatabaseError>(
+				UserSchema,
+				userRecord[0],
+				msg => createInvalidObjectInDatabaseError(userRecord, 'UserSchema', msg)
+			);
+
+      return validatedUser;
+    } catch (error) {
+      return Result.err(createUnexpectedDatabaseError(error));
+    }
+  }
+
+  async findByEmail(email: Email): Promise<Result<User, IdentityDomainError>> {
+    try {
+      const userRecord = await this.db
+        .select()
+        .from(users)
+        .where(eq(users.email, email))
+        .limit(1);
+
+      if (userRecord.length === 0) {
+        return Result.err(createUserNotFoundError(email));
+      }
+
+      const validatedUser = validate<User, InvalidObjectInDatabaseError>(
+				UserSchema,
+				userRecord[0],
+				msg => createInvalidObjectInDatabaseError(userRecord, 'UserSchema', msg)
+			);
+
+      return validatedUser;
+    } catch (error) {
+      return Result.err(createUnexpectedDatabaseError(error));
+    }
+  }
+}
---
+++ packages/backend/src/modules/identity/infrastructure/persistence/repositories/user-write.ts
@@ -0,0 +1,51 @@
+import { eq } from 'drizzle-orm';
+import { Result } from '@backend/libs/result';
+import type { DbClient } from '@backend/app/db/connector';
+import { users } from '@backend/app/db/schema';
+import {
+	createUnexpectedDatabaseError,
+	type IdentityDomainError
+} from '@backend/modules/identity/domain';
+import type { User } from '@backend/modules/identity/domain/entities/user';
+import type { UserWriteRepository } from '@backend/modules/identity/domain/repositories/user-write';
+import type { UserId } from '@backend/modules/identity/domain/value-objects/user-id';
+import { getUnixTimestampNow } from '@backend/libs/time';
+
+export class DrizzleUserWriteRepository implements UserWriteRepository {
+  constructor(private readonly db: DbClient) {}
+
+  async save(user: User): Promise<Result<void, IdentityDomainError>> {
+    try {
+      await this.db
+        .insert(users)
+        .values(user)
+        .onConflictDoUpdate({
+          target: users.id,
+          set: {
+            username: user.username,
+            email: user.email,
+            passwordHash: user.passwordHash,
+            updatedAt: user.updatedAt,
+            deletedAt: user.deletedAt ?? null,
+          },
+        });
+
+      return Result.ok(undefined);
+    } catch (error) {
+      return Result.err(createUnexpectedDatabaseError(error));
+    }
+  }
+
+  async delete(id: UserId): Promise<Result<void, IdentityDomainError>> {
+    try {
+      await this.db
+        .update(users)
+        .set({ deletedAt: getUnixTimestampNow() })
+        .where(eq(users.id, id));
+
+      return Result.ok(undefined);
+    } catch (error) {
+      return Result.err(createUnexpectedDatabaseError(error));
+    }
+  }
+}
